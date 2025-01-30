90fc22b989f03c0204e7651cce9eb6b374adaab5
bhickta <61617746+bhickta@users.noreply.github.com>
2025-01-30 11:28:05 +0000

    fix: reset insights secure mcqs

--- metrack/patches/reset_mcq.py
+++ metrack/patches/reset_mcq.py
@@ -3,8 +3,8 @@ import frappe
 def execute():
     query = """
     UPDATE `tabMCQ` 
-    SET `question_status` = NULL, `result` = NULL;
-    WHERE source like 'insights'
+    SET `question_status` = NULL, `result` = NULL
+    WHERE source LIKE '%insights%'
     """
 
     frappe.db.sql(query)
