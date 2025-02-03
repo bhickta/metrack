d7150088d46fc6fb2f3aea4a298721b9bc665637
bhickta <nishantbhickta+code@gmail.com>
2025-02-03 06:42:31 +0000

    reset mcq

--- metrack/patches/reset_mcq.py
+++ metrack/patches/reset_mcq.py
@@ -1,6 +1,18 @@
 import frappe
 
 def execute():
+    reset_mcq()
+
+def reset_mcq():
+    query = """
+    UPDATE tabMCQ 
+    SET question_status = NULL, result = NULL
+    WHERE source LIKE '%insights%'
+    """
+
+    frappe.db.sql(query)
+
+def delete_mcq():
     query = """
     DELETE FROM `tabMCQ`
     WHERE source LIKE '%insights%'
